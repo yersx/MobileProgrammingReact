0be5325d5df29d809bc16f4d8dd55d2b9b885191
Yersultan <yersx@gitlab.com>
2021-05-20 10:53:52 +0600

    MySocialNetwork init

---
+++ MySocialNetwork/App.js
@@ -0,0 +1,84 @@
+/**
+ * Yersultan Assylbekov, 20MD0123
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { FriendsContext } from './FriendsContext';
+import HomeScreen from './HomeScreen';
+import FriendsScreen from './FriendsScreen';
+
+const Stack = createStackNavigator();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      possibleFriends: [
+        'Alice',
+        'Bob',
+        'Sammy',
+      ],
+      currentFriends: [],
+    }
+  }
+
+  addFriend = (index) => {
+    const {
+      currentFriends,
+      possibleFriends,
+    } = this.state
+
+    // Pull friend out of possibleFriends
+    const addedFriend = possibleFriends.splice(index, 1)
+
+    // And put friend in currentFriends
+    currentFriends.push(addedFriend)
+
+    // Finally, update the app state
+    this.setState({
+      currentFriends,
+      possibleFriends,
+    })
+  }
+
+  render() {
+    return (
+      <FriendsContext.Provider
+        value={
+          {
+            currentFriends: this.state.currentFriends,
+            possibleFriends: this.state.possibleFriends,
+            addFriend: this.addFriend
+          }
+        }
+      >
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+            />
+            <Stack.Screen
+              name="Friends"
+              component={FriendsScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FriendsContext.Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
